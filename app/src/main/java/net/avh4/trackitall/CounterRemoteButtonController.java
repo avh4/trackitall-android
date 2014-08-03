42c9b1a6aacd88bea6fec7fd6a62cc5b3b6a45be
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-08-03 12:36:54 -0700

    Extract CounterRemoteButtonController from MyService

---
+++ app/src/main/java/net/avh4/trackitall/CounterRemoteButtonController.java
@@ -0,0 +1,40 @@
+package net.avh4.trackitall;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.widget.RemoteViews;
+
+class CounterRemoteButtonController {
+    private final String type;
+    private final String label;
+    private final int buttonId;
+    private final RemoteViews views;
+
+    CounterRemoteButtonController(RemoteViews views, final String type, int buttonId, String label) {
+        this.type = type;
+        this.label = label;
+        this.buttonId = buttonId;
+        this.views = views;
+    }
+
+    public void update(Context context) {
+        views.setTextViewText(buttonId, label + " " + Store.getCount(context, type));
+    }
+
+    public static CounterRemoteButtonController attach(Counter counter, Context context, RemoteViews views, int INC_CODE) {
+        final String type = counter.getType();
+        int buttonId = counter.getButtonId();
+        String label = context.getResources().getString(counter.getLabelId());
+        views.setOnClickPendingIntent(buttonId, PendingIntent.getBroadcast(context, INC_CODE, new Intent(type), 0));
+        context.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Store.inc(context, type);
+            }
+        }, new IntentFilter(type));
+        return new CounterRemoteButtonController(views, type, buttonId, label);
+    }
+}
--- app/src/main/java/net/avh4/trackitall/MyService.java
+++ app/src/main/java/net/avh4/trackitall/MyService.java
@@ -2,12 +2,9 @@ package net.avh4.trackitall;
 
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.IBinder;
 import android.widget.RemoteViews;
 import com.dropbox.sync.android.DbxDatastore;
@@ -18,7 +15,7 @@ import java.util.Set;
 public class MyService extends Service {
     private static final int INC_CODE = 1000;
     private static final int NID = 1;
-    private Set<Thing> things = new HashSet<Thing>();
+    private Set<CounterRemoteButtonController> remoteButtonControllers = new HashSet<CounterRemoteButtonController>();
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -30,7 +27,7 @@ public class MyService extends Service {
         final RemoteViews views = new RemoteViews(getPackageName(), R.layout.bar);
 
         for (Counter counter : Counters.ALL) {
-            things.add(Thing.attach(counter, this, views));
+            remoteButtonControllers.add(CounterRemoteButtonController.attach(counter, this, views, INC_CODE));
         }
 
         final NotificationManager mManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -42,8 +39,8 @@ public class MyService extends Service {
         DropboxStore.getStore().addSyncStatusListener(new DbxDatastore.SyncStatusListener() {
             @Override
             public void onDatastoreStatusChange(DbxDatastore dbxDatastore) {
-                for (Thing thing : things) {
-                    thing.update(MyService.this);
+                for (CounterRemoteButtonController counterRemoteButtonController : remoteButtonControllers) {
+                    counterRemoteButtonController.update(MyService.this);
                 }
                 mManager.notify(NID, notification);
             }
@@ -53,35 +50,4 @@ public class MyService extends Service {
         return START_STICKY;
     }
 
-    private static class Thing {
-        private final String type;
-        private final String label;
-        private final int buttonId;
-        private final RemoteViews views;
-
-        private Thing(RemoteViews views, final String type, int buttonId, String label) {
-            this.type = type;
-            this.label = label;
-            this.buttonId = buttonId;
-            this.views = views;
-        }
-
-        public void update(Context context) {
-            views.setTextViewText(buttonId, label + " " + Store.getCount(context, type));
-        }
-
-        public static Thing attach(Counter counter, Context context, RemoteViews views) {
-            final String type = counter.getType();
-            int buttonId = counter.getButtonId();
-            String label = context.getResources().getString(counter.getLabelId());
-            views.setOnClickPendingIntent(buttonId, PendingIntent.getBroadcast(context, INC_CODE, new Intent(type), 0));
-            context.registerReceiver(new BroadcastReceiver() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-                    Store.inc(context, type);
-                }
-            }, new IntentFilter(type));
-            return new Thing(views, type, buttonId, label);
-        }
-    }
 }
